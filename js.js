78429c3396fc4d612c7b82c63df7370bbec76026
patoxo <patricio.cr95@protonmail.com>
2022-01-12 13:43:09 -0400

    files

---
+++ js.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () =>{
+        remover()
+        panel.classList.add('active')
+    })
+    
+})
+
+function remover() {
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
